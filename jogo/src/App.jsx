09e2e3be57afad16e7886374029a891d9a64b241
kaue <130191440+kauezzzz@users.noreply.github.com>
2024-04-18 12:03:14 -0300

    up

--- jogo/src/App.jsx
+++ jogo/src/App.jsx
@@ -1,12 +1,45 @@
-import Board from "./components/Board";
+import { useState } from 'react'
+import "./App.css";
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    { id: 2,text: "Ir para a academia", category: "Pessoal", isCompleted: false },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+
+
+  ]);
+
   return (
-    <div className="App">
-      <h1>Jogo da Velha</h1>
-      <Board />
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo)=>(
+          <div className='todo'>
+            <div className='content'>
+              <p>{todo.text}</p>
+              <p className='category'>({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Todo from './components/Todo';
+
+import "./App.css";
+import TodoForm from './components/TodoForm';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    { id: 2,text: "Ir para a academia", category: "Pessoal", isCompleted: false },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+
+
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo)=>(
+        <Todo todo={todo}/>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <TodoForm />
+    </div>
   )
 }
 
